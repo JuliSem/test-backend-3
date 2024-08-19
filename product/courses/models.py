b1143f13a7d3b4aa8bf8321f2b90c7d3c65b1819
JuliSem <julia.sem295@yandex.ru>
2024-08-19 16:24:33 +0300

    Create/ change models

--- product/courses/models.py
+++ product/courses/models.py
@@ -1,13 +1,27 @@
+import os
+import sys
+
+from django.core.exceptions import ValidationError
 from django.db import models
+
+sys.path.append(os.path.join(os.getcwd(), '..'))
+
+from users.models import CustomUser
 
 
 class Course(models.Model):
     """Модель продукта - курса."""
 
-    author = models.CharField(
-        max_length=250,
+    # objects = models.Manager()
+    author = models.ForeignKey(  # вопрос?
+        CustomUser,
         verbose_name='Автор',
+        on_delete=models.CASCADE
     )
+    # author = models.CharField(
+    #     max_length=250,
+    #     verbose_name='Автор',
+    # )
     title = models.CharField(
         max_length=250,
         verbose_name='Название',
@@ -17,8 +31,18 @@ class Course(models.Model):
         auto_now_add=False,
         verbose_name='Дата и время начала курса'
     )
+    price = models.DecimalField(
+        verbose_name='Цена',
+        max_digits=6,
+        decimal_places=2
+    )
 
-    # TODO
+    def save(self, *args, **kwargs):
+        if self.price < 0:
+            raise ValidationError(
+                message='Цена на курс не может быть отрицательной!'
+            )
+        super().save(*args, **kwargs)
 
     class Meta:
         verbose_name = 'Курс'
@@ -26,12 +50,18 @@ class Course(models.Model):
         ordering = ('-id',)
 
     def __str__(self):
-        return self.title
+        return f'Курс: {self.title}'
 
 
 class Lesson(models.Model):
     """Модель урока."""
 
+    # objects = models.Manager()
+    course = models.ForeignKey(
+        Course,
+        on_delete=models.CASCADE,
+        related_name='lessons'
+    )
     title = models.CharField(
         max_length=250,
         verbose_name='Название',
@@ -41,23 +71,74 @@ class Lesson(models.Model):
         verbose_name='Ссылка',
     )
 
-    # TODO
-
     class Meta:
         verbose_name = 'Урок'
         verbose_name_plural = 'Уроки'
         ordering = ('id',)
 
     def __str__(self):
-        return self.title
+        return f'Курс: {self.course.title} | Урок: {self.title}'
 
 
 class Group(models.Model):
     """Модель группы."""
 
-    # TODO
+    title = models.CharField(
+        max_length=250,
+        verbose_name='Название',
+    )
+    course = models.ForeignKey(
+        Course,
+        verbose_name='Продукт',
+        on_delete=models.CASCADE,
+        related_name='groups'
+    )
+    count_students = models.IntegerField(
+        verbose_name='Количество студентов',
+        default=0
+    )
+    # users = models.ManyToManyField(
+    #     CustomUser,
+    #     verbose_name='Участники',
+    #     related_name='groups'
+    # )
+    # создан дополнительный класс со связью ManyToMany
 
     class Meta:
         verbose_name = 'Группа'
         verbose_name_plural = 'Группы'
         ordering = ('-id',)
+
+    def __str__(self):
+        return f'Курс: {self.course.title} | Группа: {self.title}'
+
+
+class GroupStudents(models.Model):
+    """Модель отношения студента к группе."""
+
+    group = models.ForeignKey(
+        Group,
+        verbose_name='Группа',
+        on_delete=models.CASCADE
+    )
+    user = models.ForeignKey(
+        CustomUser,
+        verbose_name='Студент',
+        on_delete=models.CASCADE
+    )
+
+    class Meta:
+        verbose_name = 'Группа студентов'
+        verbose_name_plural = 'Группы студентов'
+        ordering = ('-id',)
+        constraints = [
+            models.UniqueConstraint(
+                fields=['user', 'group'],
+                name='group_user_unique'
+            ),
+        ]
+
+    def __str__(self):
+        return (f'Курс: {self.group.course.title} '
+                f'| Группа: {self.group.title} '
+                f'| Студент: {self.user.username}')
--- product/users/models.py
+++ product/users/models.py
@@ -1,9 +1,29 @@
+import os
+import sys
+
 from django.contrib.auth.models import AbstractUser
+from django.core.exceptions import ValidationError
 from django.db import models
+
+sys.path.append(os.path.join(os.getcwd(), '..'))
+
+from courses.models import Course
+
+
+class Role(models.TextChoices):
+    TEACHER = 'TEACHER', 'Преподаватель'
+    STUDENT = 'STUDENT', 'Студент'
+
 
 class CustomUser(AbstractUser):
     """Кастомная модель пользователя - студента."""
 
+    role = models.CharField(
+        'Роль',
+        max_length=30,
+        choices=Role.choices,
+        default=Role.STUDENT
+    )
     email = models.EmailField(
         verbose_name='Адрес электронной почты',
         max_length=250,
@@ -29,21 +49,57 @@ class CustomUser(AbstractUser):
 class Balance(models.Model):
     """Модель баланса пользователя."""
 
-    # TODO
+    objects = models.Manager()
+    user = models.ForeignKey(
+        CustomUser,
+        verbose_name='Пользователь',
+        on_delete=models.CASCADE
+    )
+    balance = models.DecimalField(
+        verbose_name='Баланс (баллы)',
+        max_digits=8,
+        decimal_places=2,
+        default=1000
+    )
+
+    def save(self, *args, **kwargs):
+        if self.balance < 0:
+            raise ValidationError(
+                message='Баланс не может быть отрицательным!'
+            )
+        super().save(*args, **kwargs)
 
     class Meta:
         verbose_name = 'Баланс'
         verbose_name_plural = 'Балансы'
         ordering = ('-id',)
 
+    def __str__(self):
+        return f'Пользователь: {self.user.username} | Баланс: {self.balance}'
+
 
 class Subscription(models.Model):
     """Модель подписки пользователя на курс."""
 
-    # TODO
+    # objects = models.Manager()
+    user = models.ForeignKey(CustomUser, on_delete=models.DO_NOTHING)
+    course = models.ForeignKey(Course, on_delete=models.DO_NOTHING)
+    is_valid = models.BooleanField(
+        verbose_name='Доступ',
+        default=False
+    )
 
     class Meta:
         verbose_name = 'Подписка'
         verbose_name_plural = 'Подписки'
         ordering = ('-id',)
+        constraints = [
+            models.UniqueConstraint(
+                fields=['user', 'course'],
+                name='subscription_unique'
+            ),
+        ]
 
+    def __str__(self):
+        return (f'Пользователь: {self.user.username} '
+                f'| Курс: {self.course.title}')
