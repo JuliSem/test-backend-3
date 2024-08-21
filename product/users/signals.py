8756ec580186eee2c202bfcc837aa9ebb4d3c51e
JuliSem <julia.sem295@yandex.ru>
2024-08-21 09:33:55 +0300

    add signals for balance

---
+++ product/users/signals.py
@@ -0,0 +1,10 @@
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
+from users.models import CustomUser, Balance
+
+
+@receiver(post_save, sender=CustomUser)
+def post_save_balance(sender, instance: CustomUser, created, **kwargs):
+    if created:
+        Balance.objects.create(user=instance)
