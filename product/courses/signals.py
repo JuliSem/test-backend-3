399885729c9211ec736f914e3efdfa21c735f346
JuliSem <julia.sem295@yandex.ru>
2024-08-21 10:24:32 +0300

    create signals for subscription

--- product/courses/signals.py
+++ product/courses/signals.py
@@ -1,8 +1,8 @@
-from django.db.models import Count
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 from django.utils import timezone
-from users.models import Subscription
+
+from courses.models import Group, GroupStudents, Subscription
 
 
 @receiver(post_save, sender=Subscription)
@@ -13,5 +13,47 @@ def post_save_subscription(sender, instance: Subscription, created, **kwargs):
     """
 
     if created:
-        pass
-        # TODO
+        if instance.is_valid and instance.course.start_date > timezone.now():
+            groups = Group.objects.filter(course=instance.course).order_by(
+                'count_students'
+            )
+            if not groups:
+                new_group = Group.objects.create(
+                    titel='Группа 1',
+                    course=instance.course,
+                    count_students=1
+                )
+                GroupStudents.objects.create(
+                    user=instance.user,
+                    group=new_group
+                )
+            else:
+                group = groups.first()
+                if group.count_students == 0:
+                    GroupStudents.objects.create(
+                        user=instance.user,
+                        group=group
+                    )
+                    group.count_students = 1
+                    group.save()
+                elif groups.count() < 5:
+                    new_group = Group.objects.create(
+                        titel=f'Группа {groups.count() + 1}',
+                        course=instance.course,
+                        count_students=1
+                    )
+                    GroupStudents.objects.create(
+                        user=instance.user,
+                        group=new_group
+                    )
+                else:
+                    GroupStudents.objects.create(
+                        user=instance.user,
+                        group=groups.first()
+                    )
+                    group.count_students += 1
+                    group.save()
+        elif instance.course.start_date < timezone.now():
+            print('Курс уже начался, распределение по группам невозможно!')
+        elif not instance.is_valid:
+            print('Отсутствует доступ к курсу!')
